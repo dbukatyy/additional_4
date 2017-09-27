f09ad892abc48bc00b703f26e96f815b73f1a9d5
dbukatyy <dbukatyy@mail.ru>
2017-09-27 09:33:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 module.exports = function multiply(first, second) {
   // your solution
-  let res = [], x = 0;
-      res[0] = 0;
-  
-  for(let j = second.length - 1; j > -1; j--){
-  
-    for(let i = first.length - 1; i > -1; i--){
-      if(j != 0){
-        res[x] += first[i] * second[j];
-        res[x + 1] = 0;
-      }else {
-        res[x] += first[i] * second[j];
-      }
-    }
-    x++;
-  } 
-
-  for(let i = 0, numb = 0; i < res.lenght - 1; i++){
-    if(res[i] == 0){
-      numb = 0;
-    }else{
-      numb = Math.floor(res[i]/10);
-      res[i] = res[i] % 10;
-    }
-      res[i+1] += numb;
+ if (first.length < second.length) {
+    tmp = second;
+    second = first;
+    first = tmp;
   }
 
-  return res.reverse().join('');
-}
+  result = [];
+  resultLength = first.length + second.length;
+
+  for (z = 0; z < resultLength; z++) {
+    result[z] = 0;
+  }
+
+  for (x = 0; x < second.length; x++) {
+    for (y = 0; y < first.length; y++) {
+      result[1 + y + x] += first[y] * second[x];
+    }
+  }
+
+  for (z = resultLength - 1; z > 0; z--) {
+    if (result[z] >= 10) {
+      result[z - 1] += Math.floor(result[z] / 10);
+      result[z] %= 10;
+    }
+  }
+
+  if (result[0] == 0)
+    delete result[0];
+
+  return result.join('');
+};
